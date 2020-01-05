63f464182cf35eb5ce9713e70e483d6b59112d4e
779g <rdp@779.mx>
2020-01-05 23:08:12 +0000

    Test of the whole package

--- src/site/App.js
+++ src/site/App.js
@@ -1,28 +1,41 @@
 import { h, Fragment } from 'preact'
+import { useState } from 'preact/hooks'
 import Clock from 'components/AsyncClock'
 import QueryTester from 'components/QueryTester'
+import { AnimatedVCaret } from '@fwrlines/ds'
 
-const App = () => (
-  <>
-    <h1>Preact test</h1>
-    <h2>Includes</h2>
-    <ul>
-      <li>
+const App = () => {
+  const [active, setActive] = useState(false)
+  return (
+    <>
+      <h1>Preact test</h1>
+      <h2>
+Includes
+        <AnimatedVCaret
+          active={active}
+          setActive={setActive}
+          id="myarrow"
+          width="200px"
+        />
+      </h2>
+      <ul>
+        <li>
         SSR
-      </li>
-      <li>
+        </li>
+        <li>
         Lighthouse 100/100 ootb
-      </li>
-      <li>
+        </li>
+        <li>
         Apollo
-      </li>
-    </ul>
-    <Clock
-      thing="thing"
-      thing2="thing2"
-    />
-    <QueryTester />
-  </>
-)
+        </li>
+      </ul>
+      <Clock
+        thing="thing"
+        thing2="thing2"
+      />
+      <QueryTester />
+    </>
+  )
+}
 
 export default App
